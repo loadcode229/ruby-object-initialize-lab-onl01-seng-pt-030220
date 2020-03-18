25445d902e96e9e876327cbc551e4969d39e6b83
Adam Priest <sepone229@gmail.com>
2020-03-18 01:14:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,19 @@ class Dog
     @name, @breed = name, breed
   end
   
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+    if breed.value = 0
+      "Mutt"
+    end
+  end
+  
 end
