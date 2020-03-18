29fabb8d035d5a3d6b8890ede265faf8e2688b6f
Adam Priest <sepone229@gmail.com>
2020-03-18 01:15:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,14 @@
 class Dog
   
-  def initalize(name, breed)
-    @name, @breed = name, breed
-  end
-  
   def name=(name)
     @name = name
   end
-  
   def name
     @name
   end
-  
   def breed=(breed)
     @breed = breed
-    if breed.value = 0
-      "Mutt"
-    end
   end
-  
   def breed
     @breed
   end
